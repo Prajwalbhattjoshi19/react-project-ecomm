7581eba25f1f59c0da3939c8648f1a0d5b48b6f0
Prajwalbhattjoshi19 <155133569+Prajwalbhattjoshi19@users.noreply.github.com>
2025-06-15 08:38:13 +0530

    Update DotsGame.js adding for loop logic for rows and columns.

--- src/Pages/DotsGame.js
+++ src/Pages/DotsGame.js
@@ -1,4 +1,12 @@
 function getInitialDotCells(){
+   let columns= 7;
+   let rows = 7;
+   let output= [];
+   for(let row=1 ;row<=rows;row++){
+       for(let column=1; column<= columns; column++){
+           
+       }
+   }
    
 }
 function DotsGame(){
