1b04a04f6021ee4b1bb0c61d663eabb383f38568
Prajwalbhattjoshi19 <155133569+Prajwalbhattjoshi19@users.noreply.github.com>
2025-06-15 09:00:07 +0530

    Update DotsGame.js

--- src/Pages/DotsGame.js
+++ src/Pages/DotsGame.js
@@ -4,10 +4,18 @@ function getInitialDotCells(){
    let output= [];
    for(let row=1 ;row<=rows;row++){
        for(let column=1; column<= columns; column++){
-           
+           output.push({
+             isLastRow : row===rows,
+             isLastColumn : column=== columns,
+             color : 'black',
+             drawBottomLine : false, 
+             drawRightLine : false,
+             row,
+             column,
+           });
        }
    }
-   
+   return output;
 }
 function DotsGame(){
   const[dotCells,setDotCells] = useState();
